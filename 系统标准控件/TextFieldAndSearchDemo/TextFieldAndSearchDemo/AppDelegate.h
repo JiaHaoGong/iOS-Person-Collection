20fa546246b3526981e6f803ca83c895aee86dd6
mrhyh <mrhyh@qq.com>
2016-03-31 23:23:26 +0800

    上传读取plist等demo！

---
+++ 系统标准控件/TextFieldAndSearchDemo/TextFieldAndSearchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextFieldAndSearchDemo
+//
+//  Created by ylgwhyh on 16/3/31.
+//  Copyright © 2016年 com.ylgwhyh.TextFieldDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 系统标准控件/TextFieldAndSearchDemo/TextFieldAndSearchDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextFieldAndSearchDemo
+//
+//  Created by ylgwhyh on 16/3/31.
+//  Copyright © 2016年 com.ylgwhyh.TextFieldDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
